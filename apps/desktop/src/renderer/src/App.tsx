cd0cb72d7b0a53e7faa1639fad5fa11c358b8baf
Nuno Alexandre <nunnomalex@gmail.com>
2026-04-26 22:54:35 +0100

    feat: desktop-app init

---
+++ apps/desktop/src/renderer/src/App.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+
+function App(): React.JSX.Element {
+	return (
+		<div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+			
+		</div>
+	);
+}
+
+export default App;
